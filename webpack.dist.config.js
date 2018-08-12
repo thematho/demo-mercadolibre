bcd72587d46fdd65f3992abb06841a896d000bd0
Matias Fernandez Martinez <thematho@gmail.com>
2018-08-12 15:38:32 -0300

    Fixed Asset location Added copy-webpack-plugin to copy assets into dist folder.

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,13 +1,14 @@
-var webpack = require('webpack');
-var path    = require('path');
-var config  = require('./webpack.config');
+const webpack = require('webpack');
+const path = require('path');
+const config = require('./webpack.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 config.output = {
-  filename: '[name].bundle.js',
+  filename: '[name].[chunkhash].bundle.js',
   publicPath: '',
   path: path.resolve(__dirname, 'dist')
 };
-
+console.log(path.resolve(__dirname, 'dist'));
 config.plugins = config.plugins.concat([
 
   // Reduces bundles total size
@@ -20,7 +21,13 @@ config.plugins = config.plugins.concat([
       // angular global variable, so we should keep it unchanged
       except: ['$super', '$', 'exports', 'require', 'angular']
     }
-  })
+  }),
+  // Copy assets imagenes etc into dist folder
+  new CopyWebpackPlugin([{
+    from: 'client/assets',
+    to: 'assets'
+  }]),
+
 ]);
 
 module.exports = config;
