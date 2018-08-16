e722fdcfb2e029ce5fd90f5d9163095e21989754
Matias Fernandez Martinez <thematho@gmail.com>
2018-08-15 23:52:13 -0300

    Added Categories Navigation on result List. Minor styles added.

--- client/app/components/search-result/search-result.controller.js
+++ client/app/components/search-result/search-result.controller.js
@@ -1,19 +1,26 @@
-function SearchResultController($state, $stateParams, ItemsService) {
+function SearchResultController($state, $stateParams, ItemsService, CategoriesService) {
   'ngInject';
 
-  let $ctrl = this;
-
+  let $ctrl = this,
+    loadCategoriesPath = () => {
+      return CategoriesService
+        .get(this.results[0].category_id)
+        .then((response) => {
+          this.category = response;
+        });
+    };
   this.onItemClick = (id) => {
     $state.go('app.details', { id });
-  }
+  };
+
 
   $ctrl.$onInit = () => {
     ItemsService.search($stateParams.search)
       .then((response) => {
         this.results = response.results;
-      });
+      })
+      .then(loadCategoriesPath);
   };
-
 }
 
 export default SearchResultController;
--- client/app/components/search-result/search-result.js
+++ client/app/components/search-result/search-result.js
@@ -3,6 +3,7 @@ import uiRouter from '@uirouter/angularjs';
 import component from './search-result.component';
 import resultItemModule from './result-item/result-item';
 import itemsServiceModule from '../../services/items/items';
+import categoriesServiceModule from '../../services/categories/categories';
 import divider from './../../common/components/divider/divider';
 
 export default angular
@@ -10,7 +11,8 @@ export default angular
     uiRouter,
     divider,
     resultItemModule,
-    itemsServiceModule
+    itemsServiceModule,
+    categoriesServiceModule,
   ])
   .component('searchResult', component)
   .name;
